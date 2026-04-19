e6ec60fb4cba7d0c958440b2ba957d3daf3ee5f5
arghasings <debargha.sahakv2@gmail.com>
2026-04-19 21:25:00 +0530

    Update embeddings.py

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,11 +1,10 @@
+from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
 
 def create_vectorstore(texts):
     embeddings = HuggingFaceEmbeddings(
         model_name="all-MiniLM-L6-v2"
     )
-
-    vectorstore = Chroma.from_texts(texts, embeddings)
-
+    
+    vectorstore = FAISS.from_texts(texts, embeddings)
     return vectorstore
